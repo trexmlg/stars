3d503ab7d9d9f77ac68f370038a65f3510123a83
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-26 11:22:26 +0200

    zb jau sis ir

---
+++ clicker.js
@@ -0,0 +1,6 @@
+
+int; var clicks = 0;
+    function countClicks() {
+        clicks += 1;
+        document.getElementById("clicker").innerHTML = clicks;
+    };
--- index.php
+++ index.php
@@ -1,25 +1,41 @@
 <?php 
 
-$x = 4;
 
-echo "<p>$x</p>";
 
-echo "strada";
+
+
+
 
 $conn = new mysqli("localhost", "root", "", "stars");
 
 $results = $conn->query("SELECT * FROM users");
 
-$data = $results->fetch_all();
+$data = $results->fetch_assoc();
 
 var_dump($data);
 
-echo "<form>";
 
-echo "<input name='username'/>";
+echo "<br>";
 
-echo "<input name='password' type='password'/>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $sql = "INSERT INTO users (username, pass) VALUES('$username','$password')";
+    $conn->query($sql);
+}
+
+
+echo "<formmethod = 'POST'>";
+
+echo "<label>Lietotajvards:<input name='username' value = 'username'/></label><br>";
+
+echo "<label> Parole: <input name='password' type='password' /></label><br>";
 
 echo "<input type='submit' value='register'";
 
 echo "</form>";
+
+$mas = [6,8,10];
+
+$assoc = ["vecums" => 6, "videja_atzime" => 8, "ids" => 10];
+
